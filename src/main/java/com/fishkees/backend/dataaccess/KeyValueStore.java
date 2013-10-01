972d070333a761096010ccdb2a70c85ba2624412
mhaligowski <mhaligowski@gmail.com>
2013-10-01 17:07:07 +0200

    Integration test for getting all the FlashcardsList

--- src/main/java/com/fishkees/backend/dataaccess/KeyValueStore.java
+++ src/main/java/com/fishkees/backend/dataaccess/KeyValueStore.java
@@ -1,8 +1,9 @@
 package com.fishkees.backend.dataaccess;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public abstract class KeyValueStore<K, V> {
@@ -16,7 +17,7 @@ public abstract class KeyValueStore<K, V> {
 		return map.get(key);
 	}
 	
-	public Collection<V> all() {
-		return map.values();
+	public List<V> all() {
+		return Lists.newArrayList(map.values());
 	}
 }
--- src/test/java/com/fishkees/backend/modules/lists/dao/InMemoryFlashcardListDaoTest.java
+++ src/test/java/com/fishkees/backend/modules/lists/dao/InMemoryFlashcardListDaoTest.java
@@ -1,13 +1,9 @@
 package com.fishkees.backend.modules.lists.dao;
 
-import static com.yammer.dropwizard.testing.JsonHelpers.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import java.util.Collection;
 import java.util.List;
-
-import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.fishkees.backend.modules.lists.FlashcardListFixtures;
 import com.fishkees.backend.modules.lists.core.FlashcardList;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -28,21 +25,14 @@ public class InMemoryFlashcardListDaoTest {
 	@Test
 	public void testGetAll() throws Exception {
 		// given
-		Collection<FlashcardList> lists = flashcardList();
+		List<FlashcardList> lists = FlashcardListFixtures.all();
 		when(storage.all()).thenReturn(lists);
-		
+
 		// when
 		List<FlashcardList> result = testObj.getAll();
-		
-		assertEquals(lists,  result);
+
+		assertEquals(lists, result);
 		verify(storage).all();
-		
-	}
 
-	@SuppressWarnings("unchecked")
-	private Collection<FlashcardList> flashcardList() throws Exception {
-		return (List<FlashcardList>) fromJson(
-				jsonFixture("fixtures/lists/all.json"), List.class);
 	}
-
 }
