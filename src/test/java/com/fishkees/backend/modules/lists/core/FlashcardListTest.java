667c196c4a45a2dc7dee5faf1e3be1abc4a15326
mhaligowski <mhaligowski@gmail.com>
2013-10-09 18:50:47 +0200

    Fix serialization test

--- src/test/java/com/fishkees/backend/modules/lists/core/FlashcardListTest.java
+++ src/test/java/com/fishkees/backend/modules/lists/core/FlashcardListTest.java
@@ -1,5 +1,6 @@
 package com.fishkees.backend.modules.lists.core;
 
+import static com.yammer.dropwizard.testing.JsonHelpers.*;
 import static org.junit.Assert.*;
 
 import org.joda.time.DateTime;
@@ -8,12 +9,13 @@ import org.junit.Test;
 import com.fishkees.backend.modules.lists.FlashcardListFixtures;
 
 public class FlashcardListTest {
+	private final DateTime dateTime = new DateTime("1986-07-01T12:00Z");
 
 	@Test
 	public void serializesToJson() throws Exception {
-		final FlashcardList flashcardList = flashcardList();
-		assertEquals(FlashcardListFixtures.single().getCreateDate(),
-				flashcardList.getCreateDate());
+		final String flashcardList = asJson(flashcardList());
+		final String fixture = jsonFixture("fixtures/lists/single.json"); 
+		assertEquals(flashcardList, fixture);
 	}
 
 	@Test
@@ -38,12 +40,10 @@ public class FlashcardListTest {
 	}
 
 	private FlashcardList flashcardList() throws Exception {
-		DateTime dateTime = new DateTime("1986-07-01T12:00Z");
 		return new FlashcardList(1l, "abcd", dateTime.toDate());
 	}
 
 	private FlashcardList flashcardListWithoutId() throws Exception {
-		DateTime dateTime = new DateTime("1986-07-01T12:00Z");
 		return new FlashcardList(null, "abcd", dateTime.toDate());
 	}
 
