59ebf5f1fe7b938017ca3ca782811d837704d64d
mhaligowski <mhaligowski@gmail.com>
2013-10-20 12:37:02 +0200

    Flashcards fixtures loading

--- src/main/java/com/fishkees/backend/modules/flashcards/FlashcardsModule.java
+++ src/main/java/com/fishkees/backend/modules/flashcards/FlashcardsModule.java
@@ -1,34 +1,67 @@
 package com.fishkees.backend.modules.flashcards;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fishkees.backend.configuration.FixturesConfiguration;
 import com.fishkees.backend.modules.flashcards.core.Flashcard;
 import com.fishkees.backend.modules.flashcards.dao.FlashcardDao;
 import com.fishkees.backend.modules.flashcards.dao.FlashcardInMemoryStorage;
 import com.fishkees.backend.modules.flashcards.dao.InMemoryFlashcardDao;
-import com.fishkees.backend.modules.lists.ListsModule;
 import com.google.common.collect.Lists;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.yammer.dropwizard.json.ObjectMapperFactory;
 
 public class FlashcardsModule extends AbstractModule {
 
-	private FlashcardsModule() {
-		
+	private FixturesConfiguration config;
+
+	private FlashcardsModule(FixturesConfiguration config) {
+		this.config = config;
 	}
-	
+
 	@Override
 	protected void configure() {
 		bind(FlashcardDao.class).to(InMemoryFlashcardDao.class);
-	}
-	
-	public static FlashcardsModule simpleModule() {
-		return new FlashcardsModule();
 	}
 
 	@Provides
 	@Singleton
 	FlashcardInMemoryStorage storage() {
-		return new FlashcardInMemoryStorage(Lists.<Flashcard>newArrayList());
+		List<Flashcard> fixtures = null;
+		
+		if (this.config != null) {
+			fixtures = loadFixture(config.getFlashcardsPath());
+		} else {
+			fixtures = Lists.newArrayList();
+		}
+			
+		return new FlashcardInMemoryStorage(fixtures);
 	}
-	
+
+	public static FlashcardsModule simpleModule() {
+		return new FlashcardsModule(null);
+	}
+
+	public static FlashcardsModule moduleWithFixture(
+			FixturesConfiguration config) {
+		return new FlashcardsModule(config);
+	}
+
+	private List<Flashcard> loadFixture(String path) {
+		ObjectMapper om = new ObjectMapperFactory().build();
+		TypeReference<List<Flashcard>> tr = new TypeReference<List<Flashcard>>() {
+		};
+		try {
+			return om.readValue(new File(path), tr);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 }
--- src/test/java/com/fishkees/backend/modules/flashcards/FlashcardsIntegrationTest.java
+++ src/test/java/com/fishkees/backend/modules/flashcards/FlashcardsIntegrationTest.java
@@ -1,9 +1,14 @@
 package com.fishkees.backend.modules.flashcards;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
 
 import org.junit.Test;
 
+import com.fishkees.backend.configuration.FixturesConfiguration;
+import com.fishkees.backend.modules.flashcards.core.Flashcard;
 import com.fishkees.backend.modules.flashcards.dao.FlashcardInMemoryStorage;
 import com.fishkees.backend.modules.lists.ListsModule;
 import com.google.inject.Guice;
@@ -25,6 +30,26 @@ public class FlashcardsIntegrationTest {
 
 		// then
 		assertSame(instance1, instance2);
+	}
+
+	@Test
+	public void testStorageFixtureLoading() {
+		// given
+		FixturesConfiguration config = mock(FixturesConfiguration.class);
+		when(config.getFlashcardsPath()).thenReturn(
+				"src/test/resources/fixtures/flashcards/all.json");
+		Injector testObj = Guice.createInjector(ListsModule.simpleModule(),
+				FlashcardsModule.moduleWithFixture(config));
+
+		// when
+		List<Flashcard> actual = testObj.getInstance(
+				FlashcardInMemoryStorage.class).all();
+
+		// then
+		assertEquals(3, actual.size());
+		assertEquals("someId3", actual.get(0).getId());
+		assertEquals("someId1", actual.get(1).getId());
+		assertEquals("someId2", actual.get(2).getId());
 	}
 
 }
