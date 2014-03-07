d3f2b3a359282076f0c499ac8fe52b0db17ff35d
mhaligowski <mhaligowski@gmail.com>
2014-03-08 00:48:09 +0100

    Create mongo configuration

---
+++ src/main/java/com/fishkees/backend/configuration/MongoConfiguration.java
@@ -0,0 +1,35 @@
+package com.fishkees.backend.configuration;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yammer.dropwizard.config.Configuration;
+
+public class MongoConfiguration extends Configuration {
+	@JsonProperty
+	private String host;
+
+	@JsonProperty
+	private int port;
+
+	@JsonProperty
+	private String username;
+
+	@JsonProperty
+	private String password;
+
+	public String getHost() {
+		return host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+}
